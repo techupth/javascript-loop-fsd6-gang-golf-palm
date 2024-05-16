2064314152f58272c0b5c675ac37d2b8c9f7a00c
Krittayapornn <krittayaporn.ww@gmail.com>
2024-05-16 15:12:01 +0700

    fix: solved problem ex1-6

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,24 @@
 // Exercise #3: Find a Minimum Score
 let scores = [100, 20, 3, 1000];
-let minScore;
+let minScore = scores[0];
 // Start coding here
-
+for( let i=0; i<scores.length; i++){
+    if(scores[i] < minScore){
+        minScore = Math.min(scores[i]);
+    };
+};
 console.log(minScore);
+
+
+// เพื่อนทำแบบนี้
+// let scores = [100, 20, 3, 1000];
+// let minScore ;
+// minScore = Math.min(...scores);
+// console.log(minScore)
+
+// let scores = [100, 20, 3, 1000];
+// let minScore ;
+// for(let i in scores){
+// minScore = Math.min(i);
+// }
+// console.log(minScore)
