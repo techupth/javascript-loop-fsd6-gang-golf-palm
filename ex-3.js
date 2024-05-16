9956dd36d8ba72dbf9165dcb75b3bbfaa784d0a4
TheeradetSukprem <161497900+TheeradetSukprem@users.noreply.github.com>
2024-05-16 14:36:19 +0700

    feat: Gang Ex1-Ex6 Finish

--- ex-3.js
+++ ex-3.js
@@ -2,5 +2,8 @@
 let scores = [100, 20, 3, 1000];
 let minScore;
 // Start coding here
+for (let i = 0 ; i < scores.length; i++){
+    minScore = Math.min([i]);
+}
 
 console.log(minScore);
--- ex-5.js
+++ ex-5.js
@@ -2,3 +2,9 @@
 let companyName = "TechUp";
 
 // Start coding here
+// กำหนดตัวแปร companyName ที่เก็บชื่อบริษัท
+
+for (let i = 0; i < companyName.length; i++) {
+
+    console.log("Number", i + 1, "character is", companyName[i]);
+}
